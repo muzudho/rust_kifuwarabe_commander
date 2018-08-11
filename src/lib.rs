f7b023135fb535ad9133a06909d39d184ea738b2
muzudho <muzudho1@gmail.com>
2018-08-12 01:07:21 +0900

    用語を短く☆（＾～＾）

--- src/lib.rs
+++ src/lib.rs
@@ -27,14 +27,14 @@ impl Caret {
 /// 
 /// * `contents` - コマンドライン文字列の1行全体です。
 /// * `len` - コマンドライン文字列の1行全体の文字数です。
-pub struct CommandlineString {
+pub struct Commandline {
     pub contents: String,
     pub len: usize,
 }
-impl CommandlineString {
-    pub fn new(contents: String) -> CommandlineString {
+impl Commandline {
+    pub fn new(contents: String) -> Commandline {
         let len = contents.chars().count();
-        CommandlineString {
+        Commandline {
             contents: contents,
             len: len,
         }
@@ -52,7 +52,7 @@ impl CommandlineString {
 /// # 参考
 /// - Rustのコールバック関数について。  
 /// [2016-12-10 Idiomatic callbacks in Rust](https://stackoverflow.com/questions/41081240/idiomatic-callbacks-in-rust)
-type Callback = fn(row: &String, caret: &mut Caret);
+type Callback = fn(line: &Commandline, caret: &mut Caret);
 
 /// 何もしないコールバック関数です。
 ///
@@ -62,7 +62,7 @@ type Callback = fn(row: &String, caret: &mut Caret);
 /// * `starts` - コマンドライン文字列の次のトークンの先頭位置が入っています。
 /// * `len` - コマンドライン文字列の1行全体の文字数です。
 /// * `response` - このアプリケーションへ、このあとの対応を指示します。
-pub fn none_callback(_row: &String, _caret: &mut Caret) {
+pub fn none_callback(_line: &Commandline, _caret: &mut Caret) {
 
 }
 
@@ -72,30 +72,30 @@ pub fn none_callback(_row: &String, _caret: &mut Caret) {
 ///
 /// * `token` - 全文一致させたい文字列です。
 /// * `callback` - コールバック関数です。
-pub struct ExpectedToken {
+pub struct Token {
     pub token: String,
     pub callback: Callback,
 }
-impl ExpectedToken {
+impl Token {
     /// [token]文字列の長さだけ [starts]キャレットを進めます。
     /// [token]文字列の続きに半角スペース「 」が１つあれば、1つ分だけ読み進めます。
     ///
     /// # Arguments
     ///
-    /// * `line_str` - 読み取るコマンドライン。
+    /// * `line` - 読み取るコマンドライン。
     /// * `caret` - 読取位置。
     /// * returns - 一致したら真。
-    pub fn start_with_and_forward(&self, line_str: &CommandlineString, caret: &mut Caret) -> bool {
-        if caret.starts + self.token.len() <= line_str.len
-            && &line_str.contents[caret.starts..self.token.len()] == self.token {
+    pub fn start_with_and_forward(&self, line: &Commandline, caret: &mut Caret) -> bool {
+        if caret.starts + self.token.len() <= line.len
+            && &line.contents[caret.starts..self.token.len()] == self.token {
 
             caret.starts += self.token.len();
             // 続きにスペース「 」が１つあれば読み飛ばす
-            if 0<(line_str.len-caret.starts) && &line_str.contents[caret.starts..(caret.starts+1)]==" " {
+            if 0<(line.len-caret.starts) && &line.contents[caret.starts..(caret.starts+1)]==" " {
                 caret.starts += 1;
             }            
 
-            (self.callback)(&line_str.contents, caret);
+            (self.callback)(&line, caret);
 
             true
         } else {
@@ -109,18 +109,18 @@ impl ExpectedToken {
 /// # Arguments
 ///
 /// * `vec_row` - コマンドを複数行 溜めておくバッファーです。
-/// * `expected_token_array` - 複数件のトークン マッピングです。
+/// * `token_array` - 複数件のトークン マッピングです。
 /// * `other_callback` - トークン マッピングに一致しなかったときに呼び出されるコールバック関数です。
 pub struct Shell{
     vec_row : Vec<String>,
-    expected_token_array: Vec<ExpectedToken>,
+    token_array: Vec<Token>,
     other_callback: Callback,
 }
 impl Shell {
     pub fn new()->Shell{
         Shell{
             vec_row : Vec::new(),
-            expected_token_array: Vec::new(),
+            token_array: Vec::new(),
             other_callback: none_callback,
         }
     }
@@ -128,8 +128,8 @@ impl Shell {
     /// # Arguments
     /// 
     /// * `map` - トークンと、コールバック関数の組みです。
-    pub fn push_token_mapping(&mut self, map: ExpectedToken){
-        self.expected_token_array.push(map);
+    pub fn push_token(&mut self, map: Token){
+        self.token_array.push(map);
     }
 
     /// # Arguments
@@ -160,21 +160,21 @@ impl Shell {
     /// 強制終了する場合は、 [Ctrl]+[C] を入力してください。
     pub fn run(&mut self) {
         loop{
-            let line_str : CommandlineString;
+            let line : Commandline;
             if self.is_empty() {
-                let mut line = String::new();
+                let mut line_string = String::new();
                 // コマンド プロンプトからの入力があるまで待機します。
-                io::stdin().read_line(&mut line)
+                io::stdin().read_line(&mut line_string)
                     .ok() // read_line が返す Resultオブジェクト の okメソッド。
                     .expect("info Failed to read_line"); // OKでなかった場合のエラーメッセージ。
 
                 // 末尾の 改行 を除きます。前後の空白も消えます。
-                line = line.trim().parse().ok().expect("info Failed to parse");
+                line_string = line_string.trim().parse().ok().expect("info Failed to parse");
 
-                line_str = CommandlineString::new(line);
+                line = Commandline::new(line_string);
             } else {
                 // バッファーの先頭行です。
-                line_str = CommandlineString::new(self.pop_row());
+                line = Commandline::new(self.pop_row());
             }
 
 
@@ -182,8 +182,8 @@ impl Shell {
             let mut caret = Caret::new();
             let mut is_done = false;
 
-            for element in self.expected_token_array.iter() {
-                if element.start_with_and_forward(&line_str, &mut caret) {
+            for element in self.token_array.iter() {
+                if element.start_with_and_forward(&line, &mut caret) {
                     is_done = true;
                     break;
                 }
@@ -191,7 +191,7 @@ impl Shell {
 
             // 何とも一致しなかったら実行します。
             if !is_done {
-                (self.other_callback)(&line_str.contents, &mut caret);
+                (self.other_callback)(&line, &mut caret);
             }
 
             if caret.quits {
